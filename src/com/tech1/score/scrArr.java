53341966361d86df25f3bfd7ab9c605ecf214252
hyunb915 <hyunb915@naver.com>
2020-08-19 17:43:28 +0900

    score_step2

--- src/com/tech1/score/scrArr.java
+++ src/com/tech1/score/scrArr.java
@@ -7,18 +7,28 @@ public class scrArr {
 		Scanner sc = new Scanner(System.in);
 		String[] student = { "bob", "jack", "kim", "jay", "ku" };
 		int[] score = new int[5];
-		float avg = 0;
+		float avg = 0f;
 		int Totalscore = 0;
-		for (int i = 0; i < score.length; i++) {
-			score[i] = sc.nextInt();
-			Totalscore += score[i];
+		while(true) {
+			for (int i = 0; i < score.length; i++) {
+				System.out.println("점수입력");
+				score[i] = sc.nextInt();
+				if (score[i] == -1) {
+					System.out.println("종료");
+					return;
+				}
+				else if (score[i] < 50 || score[i] > 100) {
+					System.out.println("다시입력");
+					i--;
+				}
+				Totalscore += score[i];
+			}
+			avg = (float) Totalscore / 5;
+			for (int i = 0; i < score.length; i++) {
+				System.out.println(student[i] + ": " + score[i]);
+			}
+			System.out.println("total:" + Totalscore + " avg:" + avg);
 		}
-		avg = Totalscore / 5;
-		for (int i = 0; i < score.length; i++) {
-			System.out.println(student[i] + ": " + score[i]);
-		}
-		System.out.println("total:"+Totalscore+ " avg:" +avg);
-		sc.close();
 
 	}
 
