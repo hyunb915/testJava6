af965cb298410815f2f9a9999ec7cd5a6ce9afd4
hyunb915 <hyunb915@naver.com>
2020-08-19 17:22:51 +0900

    score_step1.1

--- src/com/tech1/score/scrArr.java
+++ src/com/tech1/score/scrArr.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class scrArr {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String[] student = {"bob", "jack", "kim","jay","ku"};
+		String[] student = { "bob", "jack", "kim", "jay", "ku" };
 		int[] score = new int[5];
-		int avg = 0;
+		float avg = 0;
 		int Totalscore = 0;
 		for (int i = 0; i < score.length; i++) {
-			score[i] = sc.nextInt(); 
+			score[i] = sc.nextInt();
+			Totalscore += score[i];
 		}
+		avg = Totalscore / 5;
 		for (int i = 0; i < score.length; i++) {
-			System.out.println(student[i]+ ": "+score[i]);
+			System.out.println(student[i] + ": " + score[i]);
 		}
-		
+		System.out.println("total:"+Totalscore+ " avg:" +avg);
+		sc.close();
+
 	}
 
 }
